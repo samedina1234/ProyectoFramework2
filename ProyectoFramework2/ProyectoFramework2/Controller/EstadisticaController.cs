ae15471e95bd8ea1d9a831243bc71bee9f380d86
samedina1234 <samedina@tes.edu.ec>
2025-02-27 15:45:13 -0500

    Estadistica ya funciona parcialmente

---
+++ ProyectoFramework2/ProyectoFramework2/Controller/EstadisticaController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFramework2.Data;
+using ProyectoFramework2.Shared.Entities;
+
+namespace ProyectoFramework2.Controller
+{
+    [Route("api/estadisticas")]
+    [ApiController]
+    public class EstadisticaController : ControllerBase
+    {
+        private readonly HabitosContext _context;
+
+        public EstadisticaController(HabitosContext context)
+        {
+            _context = context;
+        }
+
+        // Obtener hábitos completados por usuario
+        [HttpGet("usuario/{usuarioId}")]
+        public async Task<ActionResult<IEnumerable<RegistroHabito>>> GetHabitosCompletadosPorUsuario(int usuarioId)
+        {
+            var registros = await _context.RegistroHabitos
+                .Where(r => r.UsuarioId == usuarioId)
+                .ToListAsync();
+
+            return registros;
+        }
+
+        // Obtener hábitos completados en un rango de fechas
+        [HttpGet("fechas/{fechaInicio}/{fechaFin}")]
+        public async Task<ActionResult<IEnumerable<RegistroHabito>>> GetHabitosCompletadosPorFecha(DateTime fechaInicio, DateTime fechaFin)
+        {
+            var registros = await _context.RegistroHabitos
+                .Where(r => r.FechaCompletado >= DateOnly.FromDateTime(fechaInicio) && r.FechaCompletado <= DateOnly.FromDateTime(fechaFin))
+                .ToListAsync();
+
+            return registros;
+        }
+
+        // Contar cuantas veces se completó un habito por usuario.
+        [HttpGet("conteo/{usuarioId}/{habitoId}")]
+        public async Task<ActionResult<int>> GetConteoHabito(int usuarioId, int habitoId)
+        {
+            var conteo = await _context.RegistroHabitos
+                .Where(r => r.UsuarioId == usuarioId && r.HabitoId == habitoId)
+                .SumAsync(r => r.VecesCompletado);
+
+            return conteo;
+        }
+    }
+}
