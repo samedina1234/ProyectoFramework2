15eff21eb6daf647ea470c2d6a005be07cc1406a
Samircitoxd28 <kleber.mosquera@outlook.com>
2025-03-03 14:51:52 -0500

    implementacion de cambios femenino, masculino y calcular edad

---
+++ ProyectoFramework2/ProyectoFramework2/Controller/Homecontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoFramework2.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Homecontroller : ControllerBase
+    {
+
+    }
+}
